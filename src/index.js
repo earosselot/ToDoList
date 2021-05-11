0fbdd40c967906f6d62b4ea4f8369f5d2388d206
earosselot <earosselot@gmail.com>
2021-05-11 11:21:33 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import {ToDoNote} from './todonotes';
+import * from './todonotes';
 
 console.log('done');
 let n1 = new ToDoNote('aassad', 'basdsad' ,'asdasc', 3)
 console.log(n1)
+n1.setPriority(2);
+
+console.log('123');
