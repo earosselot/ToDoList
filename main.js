af409af0c99f88af93a103e22239df60afdb93d8
earosselot <earosselot@gmail.com>
2021-05-15 03:58:32 -0300

    add project modal, form and submit working. add note modal and form working. add note form submit todo.

--- main.js
+++ main.js
@@ -10,16 +10,20 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "domNote": () => (/* binding */ domNote)
+/* harmony export */   "Note": () => (/* binding */ Note),
+/* harmony export */   "Project": () => (/* binding */ Project)
 /* harmony export */ });
+// todo: agregar fechas
 
-const domNote = (id, title, description, priority) => {
+const Note = (id, title, description, priority) => {
+    // Creates an object containing a note DOM element
+
     const note = document.createElement('div');
     note.className = 'todo-note';
 
     const completeChkbx = document.createElement('input');
     completeChkbx.setAttribute('type', 'checkbox');
-    completeChkbx.setAttribute('id', id);
+    completeChkbx.setAttribute('id', `note-${id}`);
     // todo: ver cual de los dos que siguen es realmente necesario.
     completeChkbx.setAttribute('name', 'complete');
     completeChkbx.setAttribute('value', 'complete');
@@ -38,27 +42,42 @@ const domNote = (id, title, description, priority) => {
     note.appendChild(completeChkbxLabel);
     note.appendChild(descriptionDiv);
 
+    // todo: agregar un boton de eliminar nota
+    // todo: agregar event listeners
+
     return {note}
 }
 
-// todo: agregar un boton de eliminar nota
-// todo: agregar event listeners
 
+const Project = (id, name) => {
+    // creates an object containing a Project DOM element
 
-        // method binded to the object (every object has his own) this prevent this "loosing" on event listeners
-        // this.element.onclick = this.onClick.bind(this);
+    const project = document.createElement('div');
+    project.className = 'project';
+    project.setAttribute('id', `proj-${id}`);
 
+    const projectTitle = document.createElement('div');
+    projectTitle.className = 'project-title';
+    projectTitle.innerText = name;
 
-    // onClick() {
-    //     this.element.classList.toggle('clicked');
-    //
-    // }
+    const projectNotes = document.createElement('div');
+    projectNotes.className = 'project-notes';
 
-    // This will also prevent "this loosing"
-    // toggleComplete = (event) => {
-    //     // this.complete = checked (?)
-    //     event.stopPropagation();  // this line will prevent triggereing card event
-    // }
+    const addNoteContainer = document.createElement('div');
+    addNoteContainer.className = 'add-note-container';
+
+    const addNoteBtn = document.createElement('button');
+    addNoteBtn.setAttribute('id', `add-note-proj-${id}`);
+    addNoteBtn.textContent = 'Add Note';
+
+    addNoteContainer.appendChild(addNoteBtn);
+
+    project.appendChild(projectTitle);
+    project.appendChild(projectNotes);
+    project.appendChild(addNoteContainer);
+
+    return {project}
+}
 
 
 
@@ -101,13 +120,14 @@ const domNote = (id, title, description, priority) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "createNote": () => (/* binding */ createNote)
+/* harmony export */   "Note": () => (/* binding */ Note),
+/* harmony export */   "Project": () => (/* binding */ Project)
 /* harmony export */ });
 
 
 let nextId = 0;
 
-const createNote = (title, description, _dueDate, priority, project) => {
+const Note = (title, description, _dueDate, priority, project) => {
 
     let id = nextId++;
 
@@ -131,6 +151,14 @@ const createNote = (title, description, _dueDate, priority, project) => {
     let complete = false;
 
     return {id, title, description, creationDate, dueDate, priority, project, complete}
+}
+
+let projId = 0;
+
+const Project = (name) => {
+    let id = projId++;
+    let notes = []
+    return {id, name, notes}
 }
 
 
@@ -209,10 +237,22 @@ __webpack_require__.r(__webpack_exports__);
 
 
 // import { compareDesc } from 'date-fns';
-
+// todo: agregar el tema de las fechas
 
 // notes dictionary have keys as projects and values are note objects.
-let notes = {};
+let projects = {};
+
+// projects = [{
+//     'id': projectObject.id,
+//     'project': projectObject,
+//     'notes': [note1, note2, note3,...]
+//     },
+//     {
+//     'id': projectObject.id,
+//     'project': projectObject,
+//     'notes': [note1, note2, note3,...]
+//     },
+// ]
 
 // todo: hacer el form
 // const form = document.querySelector('FORMULARIO');
@@ -230,12 +270,111 @@ let notes = {};
 //     // todo: 4- agregar nota al DOM
 // })
 
-const note1 = _todos_js__WEBPACK_IMPORTED_MODULE_0__.createNote('title1', 'description', '10/05/2022', 2, 'default');
+// note creation
+const note1 = _todos_js__WEBPACK_IMPORTED_MODULE_0__.Note('title1', 'description', '10/05/2022', 2, 'default');
 
-const note2 = _todos_js__WEBPACK_IMPORTED_MODULE_0__.createNote('title2', 'descriasdadption', '10/05/2022', 2, 'default1');
+// project creation
+const project1 = _todos_js__WEBPACK_IMPORTED_MODULE_0__.Project('default');
 
-const note1Dom = _dom_js__WEBPACK_IMPORTED_MODULE_1__.domNote('23', 'newnote', 'things', '2');
-const div = document.querySelector('.project-notes');
+// add note to project
+project1.notes.push(note1);
+
+// create DOM Project
+const project1Dom = _dom_js__WEBPACK_IMPORTED_MODULE_1__.Project(project1.id, project1.name);
+
+// create DOM notes
+const note1Dom = _dom_js__WEBPACK_IMPORTED_MODULE_1__.Note(note1.id, note1.title, note1.description, note1.priority);
+
+// add dom-note to dom-project
+project1Dom.project.appendChild(note1Dom.note);
+
+// add dom-project (with notes) to project container in the last position
+const projectContainer = document.querySelector('.project-container');
+projectContainer.appendChild(project1Dom.project);
+
+// ------------------------ ADD NOTE MODAL ------------------------
+const addNoteModal = document.getElementById('add-note-modal');
+const addNoteModalSpan = document.getElementById('add-note-modal-close');
+
+addNoteModalSpan.onclick = () => addNoteModal.style.display = 'none';
+window.onclick = (event) => {
+    if (event.target == addNoteModal) {
+        addNoteModal.style.display = 'none';
+    }
+}
+
+function getNoteFormData(formData) {
+    const title = formData.get('note-title');
+    const description = formData.get('note-description');
+    const dueDate = formData.get('due-date');
+    const priority = formData.get('priority');
+    return {title, description, dueDate, priority}
+}
+
+
+// -------------------- ADD NOTE FORM SUBMIT ---------------------
+function noteSubmit(event) {
+    const formData = new FormData(event.target);
+    const data = getNoteFormData(formData);
+    const note = _todos_js__WEBPACK_IMPORTED_MODULE_0__.Note(data.title, data.description, data.dueDate, data.priority, currentProjectId);
+
+    // project.notes.push(note); todo: arreglar esto para que siga la estructura seteada al principio
+    // todo: agregar nota al dom. para esto hay que seleccionar de alguna manera el proyecto (currentProjectId)
+
+    event.preventDefault();
+    addNoteModal.style.display = 'none';
+}
+
+const addNoteForm = document.forms['new-note'];
+addNoteForm.addEventListener('submit', noteSubmit);
+
+
+// ---------------------- ADD PROJECT MODAL ----------------------
+const addPrjModal = document.getElementById('add-prj-modal');
+const addPrjModalBtn = document.getElementById('open-add-prj-modal-btn');
+const addPrjModalSpan = document.getElementById('add-project-modal-close');
+
+// when btn click opens the modal, closes it when click span or outside modal
+addPrjModalBtn.onclick = () => addPrjModal.style.display = 'block';
+addPrjModalSpan.onclick = () => addPrjModal.style.display = 'none';
+window.onclick = (event) => {
+    if (event.target == addPrjModal) {
+        addPrjModal.style.display = 'none';
+    }
+}
+
+// this variable will store the Id of the project where the last note is being added
+let currentProjectId;
+
+// ------------------- ADD PROJECT FORM SUBMIT -------------------
+// New Project Submit handler
+function prjSubmit(event) {
+    // creates an instance of FormData
+    const formData = new FormData(event.target);
+    // the get method from the class FormData returns the input by HTML name property
+    const prjTitle = formData.get('prj-title');
+    const todoProject = _todos_js__WEBPACK_IMPORTED_MODULE_0__.Project(prjTitle);
+    // save the projects inside an object
+    projects[todoProject.id] = todoProject;
+    const projectDom = _dom_js__WEBPACK_IMPORTED_MODULE_1__.Project(todoProject.id, todoProject.name);
+
+    const addNoteBtn = projectDom.project.querySelector(`#add-note-proj-${todoProject.id}`);
+    addNoteBtn.addEventListener('click', (event) => {
+        addNoteModal.style.display = 'block';
+        // project id handling, in order to know in which project was launched
+        const projDomId = event.target.getAttribute('id').split('-');
+        currentProjectId = projDomId[3];
+    })
+
+    projectContainer.appendChild(projectDom.project);
+    event.preventDefault();
+    // hide modal after submit
+    addPrjModal.style.display = 'none';
+}
+
+const addPrjForm = document.forms['new-prj'];
+addPrjForm.addEventListener('submit', prjSubmit)
+
 
 
 // 5. The look of the User Interface is up to you, but it should be able to do the following:
